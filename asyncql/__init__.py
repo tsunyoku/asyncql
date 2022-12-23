53df300f5f186a6de971915a84e69bbacc32c6f1
James <tsunyoku@gmail.com>
2022-12-23 19:52:50 +0000

    chore: update package version

--- asyncql/__init__.py
+++ asyncql/__init__.py
@@ -2,5 +2,5 @@ from asyncql.models.connection import Connection
 from asyncql.models.database import Database
 from asyncql.models.transaction import Transaction
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __all__ = ["Database", "Connection", "Transaction"]
